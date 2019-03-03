512e1d456e59b74fc2448594665d5b3426375061
Vinicius Gomez <viniciusgomez@icloud.com>
2019-03-03 11:22:03 -0300

    Adicionando mais copslevel.

--- Banco de Dados e Outros/SQL/cxpf_db.sql
+++ Banco de Dados e Outros/SQL/cxpf_db.sql
@@ -83,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `players` (
   `pid` varchar(17) NOT NULL,
   `cash` int(100) NOT NULL DEFAULT '0',
   `bankacc` int(100) NOT NULL DEFAULT '0',
-  `coplevel` enum('0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17') NOT NULL DEFAULT '0',
+  `coplevel` enum('0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40') NOT NULL DEFAULT '0',
   `mediclevel` enum('0','1','2','3','4','5','6','7','8','9') NOT NULL DEFAULT '0',
   `civ_licenses` text NOT NULL,
   `cop_licenses` text NOT NULL,
